e75462355e3ccda743dc8bf3cf1d82feef1fb55d
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-31 20:56:01 +0530

    Update spam.py

--- main/plugins/spam.py
+++ main/plugins/spam.py
@@ -11,6 +11,9 @@ async def u(event):
         await event.reply("You're not authorized to use me!")
         return
     if (str(event.text)).lower().startswith("spam:"): 
+        if event.is_private and not f'{event.sender_id}' in au:
+            await event.reply("You're not authorized to use me!")
+            return
         reply = await event.get_reply_message()
         if not reply:
             await event.reply("Reply to any message.")
