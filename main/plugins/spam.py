3a7c87666d654941e19b53c3857fcf9239a15672
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-31 20:32:09 +0530

    Update and rename empty to spam.py

---
+++ main/plugins/spam.py
@@ -0,0 +1,54 @@
+#ChauhanMahesh/Vasusen/COL/DroneBots
+
+from .. import bot, AUTH
+from telethon import events
+
+@Drone.on(events.NewMessage(incoming=True))
+async def u(event):
+    au = AUTH.split(",")
+    if event.is_private and not f'{event.sender_id}' in au:
+        await event.reply("You're not authorized to use me!")
+        return
+    if (str(event.text)).lower().startswith("spam:"): 
+        reply = await event.get_reply_message()
+        if not reply:
+            await event.reply("Reply to any message.")
+            return
+        if not len((event.text).split(" ")) == 4:
+            await event.reply("**ERROR:** Incorrect format\n\nFormat: `spam: sleep_time spam_range chat`")
+            return
+        sleep = 1
+        total = 0
+        chat = 123456789
+        try:
+            sleep = int((event.text).split(" ")[1])
+            total = int((event.text).split(" ")[2])
+            if int((event.text).split(" ")[3]) == 0:
+                chat = event.chat_id
+            else:
+                chat = int((event.text).split(" ")[3]) 
+        except ValueError:
+            await event.reply("Sleep_time and spam_range should be integers.")
+        try:
+            for i in range(total):
+                if sleep == 0:
+                    await event.client.send_message(chat, reply) 
+                else:
+                    await event.client.send_message(chat, reply) 
+                    await asyncio.sleep(sleep)
+            except FloodWaitError as fw:
+                await asyncio.sleep(fw.seconds + 5)
+                if sleep == 0:
+                    await event.client.send_message(chat, reply) 
+                else:
+                    await event.client.send_message(chat, reply) 
+                    await asyncio.sleep(sleep)
+        except Exception as e:
+            await event.client.send_message(event.chat_id, f"**SPAM FAILED**\n\nError: {str(e)}")
+                
+            
+            
+            
+            
+            
+            
